2a71508ac319abc93219e4dbcfb109d8c5fb2ce3
Anna Trifonova <3809322999@ukr.net>
2023-07-07 18:08:30 +0300

    Add CreatePostScreen

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,21 @@
 import { Pressable, StyleSheet, Text } from 'react-native';
 
-export const Button = ({ onPressFunction, text }) => {
+export const Button = ({ onPressFunction, text, disabled }) => {
 	return (
 		<Pressable
 			onPress={onPressFunction}
+			disabled={disabled}
 			style={({ pressed }) => [
 				{
 					backgroundColor: pressed ? '#FF8C00' : '#FF6C00',
 				},
 				styles.btn,
+				disabled && styles.btnDisabled,
 			]}
 		>
-			<Text style={styles.btnText}>{text}</Text>
+			<Text style={[styles.btnText, disabled && styles.btnTextDisabled]}>
+				{text}
+			</Text>
 		</Pressable>
 	);
 };
@@ -22,9 +26,15 @@ const styles = StyleSheet.create({
 		paddingVertical: 16,
 		borderRadius: 100,
 	},
+	btnDisabled: {
+		backgroundColor: '#F6F6F6',
+	},
 	btnText: {
 		color: '#ffffff',
 		textAlign: 'center',
 		fontFamily: 'Roboto-Regular',
+	},
+	btnTextDisabled: {
+		color: '#BDBDBD',
 	},
 });
--- src/screens/CreatePostsScreen.jsx
+++ src/screens/CreatePostsScreen.jsx
@@ -1,11 +1,139 @@
-import { Text, View } from 'react-native';
+import { Pressable } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { MaterialIcons, Feather } from '@expo/vector-icons';
+import { useState } from 'react';
+import { Button } from '../components/Button';
 
 const CreatePostsScreen = () => {
+	const [isName, setIsName] = useState(false);
+	const [isLocation, setIsLocation] = useState(false);
+
+	const handlePublish = () => {
+		console.log('publish');
+	};
+
 	return (
-		<View>
-			<Text>CreatePostsScreen</Text>
+		<View style={styles.container}>
+			<Pressable style={styles.imgContainer}>
+				<View style={styles.iconContainer}>
+					<MaterialIcons name="camera-alt" size={24} color="#BDBDBD" />
+				</View>
+			</Pressable>
+			<Text style={styles.text}>Завантажте фото</Text>
+			<TextInput
+				placeholder="Назва"
+				placeholderTextColor="#BDBDBD"
+				onChangeText={text =>
+					text.length === 0 ? setIsName(false) : setIsName(true)
+				}
+				style={[styles.input, !isName && styles.textPlaceholder]}
+			/>
+			<View style={styles.inputField}>
+				<Feather name="map-pin" size={24} style={styles.inputIcon} />
+				<TextInput
+					placeholder="Місцевість..."
+					placeholderTextColor="#BDBDBD"
+					onChangeText={text =>
+						text.length === 0 ? setIsLocation(false) : setIsLocation(true)
+					}
+					style={[
+						styles.input,
+						!isLocation && styles.textPlaceholder,
+						styles.inputTextWithIconLeft,
+					]}
+				/>
+			</View>
+			<Button
+				text="Опубліковати"
+				onPressFunction={handlePublish}
+				disabled={true}
+			/>
+			<Pressable
+				style={[styles.btnDelete, styles.btnDeleteDisable]}
+				onPress={handlePublish}
+				disabled={true}
+			>
+				<Feather
+					name="trash-2"
+					size={24}
+					style={[styles.iconDeleteActive, styles.iconDeleteDisable]}
+				/>
+			</Pressable>
 		</View>
 	);
 };
 
 export default CreatePostsScreen;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		paddingHorizontal: 16,
+		paddingTop: 32,
+		paddingBottom: 22,
+		backgroundColor: '#FFFFFF',
+	},
+	imgContainer: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginBottom: 8,
+		height: 240,
+		backgroundColor: '#E8E8E8',
+	},
+	iconContainer: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		width: 60,
+		height: 60,
+		backgroundColor: '#FFFFFF',
+		borderRadius: 50,
+	},
+	text: {
+		marginBottom: 32,
+		color: '#BDBDBD',
+		fontFamily: 'Roboto-Regular',
+		fontSize: 16,
+	},
+	input: {
+		color: '#212121',
+		fontFamily: 'Roboto-Medium',
+		fontSize: 16,
+		paddingTop: 16,
+		paddingBottom: 15,
+		borderBottomColor: '#E8E8E8',
+		borderBottomWidth: 1,
+	},
+	inputField: {
+		justifyContent: 'center',
+		marginBottom: 32,
+	},
+	inputTextWithIconLeft: {
+		paddingLeft: 28,
+	},
+	textPlaceholder: {
+		fontFamily: 'Roboto-Regular',
+	},
+	inputIcon: {
+		color: '#BDBDBD',
+		position: 'absolute',
+	},
+	btnDelete: {
+		marginTop: 'auto',
+		alignSelf: 'center',
+		justifyContent: 'center',
+		alignItems: 'center',
+		width: 70,
+		height: 40,
+		borderRadius: 50,
+		backgroundColor: '#FF6C00',
+	},
+	btnDeleteDisable: {
+		backgroundColor: '#F6F6F6',
+	},
+	iconDeleteActive: {
+		color: '#FFFFFF',
+	},
+	iconDeleteDisable: {
+		color: '#BDBDBD',
+	},
+});
