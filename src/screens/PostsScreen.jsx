11c776c7d4c68ad0ad041a91b1a881ae028f95f6
Anna Trifonova <3809322999@ukr.net>
2023-07-08 15:52:42 +0300

    Change PostScreen nd ProfileScreen

--- src/screens/PostsScreen.jsx
+++ src/screens/PostsScreen.jsx
@@ -1,11 +1,50 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { useUser } from '../hooks/userContext';
 
 const PostScreen = () => {
+	const { userName, userEmail } = useUser();
+
 	return (
-		<View>
-			<Text>PostScreen</Text>
+		<View style={styles.container}>
+			<View style={styles.userWrapper}>
+				<View style={styles.imageContainer}></View>
+				<View>
+					{userName && <Text style={styles.userName}>{userName}</Text>}
+					{userEmail && <Text style={styles.userEmail}>{userEmail}</Text>}
+				</View>
+			</View>
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		paddingHorizontal: 16,
+		paddingTop: 32,
+		backgroundColor: '#FFFFFF',
+	},
+	userWrapper: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		columnGap: 8,
+	},
+	imageContainer: {
+		width: 60,
+		height: 60,
+		borderRadius: 16,
+		backgroundColor: '#E8E8E8',
+	},
+	userName: {
+		color: '#212121',
+		fontFamily: 'Roboto-Bold',
+		fontSize: 13,
+	},
+	userEmail: {
+		color: 'rgba(33, 33, 33, 0.80)',
+		fontFamily: 'Roboto-Regular',
+		fontSize: 11,
+	},
+});
 
 export default PostScreen;
--- src/screens/ProfileScreen.jsx
+++ src/screens/ProfileScreen.jsx
@@ -1,11 +1,97 @@
-import { Text, View } from 'react-native';
+import {
+	StyleSheet,
+	ImageBackground,
+	SafeAreaView,
+	Text,
+	View,
+	Pressable,
+} from 'react-native';
+import { useUser } from '../hooks/userContext';
+import { AntDesign, Feather } from '@expo/vector-icons';
 
 const ProfileScreen = () => {
+	const { userName, logOut } = useUser();
+
+	const handleAddImg = () => {
+		console.log('add img');
+	};
+
 	return (
-		<View>
-			<Text>ProfileScreen</Text>
+		<View style={styles.container}>
+			<ImageBackground
+				source={require('../assets/images/bg.jpg')}
+				resizeMode="cover"
+				style={styles.backgroundImg}
+			>
+				<SafeAreaView style={styles.container}>
+					<View style={styles.inner}>
+						<View style={styles.imgBox}>
+							<Pressable onPress={handleAddImg} style={styles.addBtnWrapper}>
+								<AntDesign name="plus" size={18} color="#FF6C00" />
+							</Pressable>
+						</View>
+						<Pressable onPress={() => logOut()} style={styles.btnLogOut}>
+							<Feather name="log-out" size={24} color="#BDBDBD" />
+						</Pressable>
+						<Text style={styles.userName}>{userName ? 'Name' : userName}</Text>
+					</View>
+				</SafeAreaView>
+			</ImageBackground>
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	backgroundImg: {
+		flex: 1,
+	},
+	inner: {
+		flex: 1,
+		marginTop: 147,
+		borderTopLeftRadius: 25,
+		borderTopRightRadius: 25,
+		paddingHorizontal: 16,
+		paddingTop: 92,
+		backgroundColor: '#FFFFFF',
+	},
+	imgBox: {
+		position: 'absolute',
+		top: -60,
+		width: 120,
+		height: 120,
+		borderRadius: 16,
+		alignSelf: 'center',
+		backgroundColor: '#F6F6F6',
+	},
+	addBtnWrapper: {
+		position: 'absolute',
+		right: -12,
+		top: 84,
+		width: 25,
+		height: 25,
+		borderWidth: 1,
+		borderColor: '#FF6C00',
+		borderRadius: 50,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#FFFFFF',
+	},
+	btnLogOut: {
+		position: 'absolute',
+		top: 22,
+		right: 16,
+	},
+	userName: {
+		marginBottom: 33,
+		color: '#212121',
+		textAlign: 'center',
+		fontFamily: 'Roboto-Medium',
+		fontSize: 30,
+		letterSpacing: 0.3,
+	},
+});
 
 export default ProfileScreen;
